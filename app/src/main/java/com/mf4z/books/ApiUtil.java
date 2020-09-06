511a64c1ffa109312d7ff208545d583a9e578b07
Marwan Mai <emef4z@gmail.com>
2020-09-06 22:48:25 +0100

    Building URL and connecting to Api

--- app/src/main/java/com/mf4z/books/ApiUtil.java
+++ app/src/main/java/com/mf4z/books/ApiUtil.java
@@ -1,4 +1,58 @@
 package com.mf4z.books;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.SoftReference;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ApiUtil {
+
+    private ApiUtil(){}
+
+    public static final String BASE_API_URL =
+            "https://www.googleapis.com/books/v1/volumes";
+
+    public static URL buildUrl(String title){
+        String fullUrl = BASE_API_URL + "?q=" +title;
+        URL url = null;
+        try {
+            url = new URL(fullUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    public static String getJSON(URL url) throws IOException {
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream stream = connection.getInputStream();
+            Scanner scanner = new Scanner(stream);
+            scanner.useDelimiter("\\A");
+            boolean hasData = scanner.hasNext();
+            if (hasData){
+                return scanner.next();
+            }
+            else{
+                return null;
+            }
+
+        }catch (Exception e){
+
+            Log.d("Error",e.toString());
+            return null;
+        }
+        finally {
+            connection.disconnect();
+        }
+    }
 }
